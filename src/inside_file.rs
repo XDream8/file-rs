b331bf8643528faa7adfd7e4f1d033ea7c3232ee
Kirsten Freitag <kirsten.freitag@proton.me>
2023-08-11 05:45:46 +0000

    Moved script type information into an enum (#7)
    I have extracted the script type texts to prevent mistakes in doubled string content.
    
    Reviewed-on: https://codeberg.org/XDream8/file-rs/pulls/7
    Co-authored-by: Kirsten Freitag <kirsten.freitag@proton.me>
    Co-committed-by: Kirsten Freitag <kirsten.freitag@proton.me>

--- src/inside_file.rs
+++ src/inside_file.rs
@@ -4,6 +4,35 @@ use std::{
     path::Path,
 };
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ScriptType {
+    Bash,
+    Tcsh,
+    Csh,
+    Yash,
+    Ash,
+    Ksh,
+    Zsh,
+    Sh,
+    Python,
+}
+
+impl ScriptType {
+    pub fn as_str(self) -> &'static str {
+        match self {
+            Self::Bash => "Bourne-Again shell",
+            Self::Tcsh => "Tenex C shell",
+            Self::Csh => "C shell",
+            Self::Yash => "Yet-Another shell",
+            Self::Ash => "Neil Brown's ash",
+            Self::Ksh => "Korn shell",
+            Self::Zsh => "Paul Falstad's zsh",
+            Self::Sh => "POSIX shell",
+            Self::Python => "Python",
+        }
+    }
+}
+
 #[inline(always)]
 pub fn get_type_from_shebang(path: &Path) -> Option<String> {
     // Open the file in read-only mode (if we encounter an error it returns None).
@@ -31,21 +60,21 @@ pub fn get_type_from_shebang(path: &Path) -> Option<String> {
         first_line.splitn(2, ' ').last().unwrap_or_default()
     };
 
-    Some(evaluate_shebang(shebang).to_owned())
+    Some(evaluate_shebang(shebang))
 }
 
 #[inline]
 fn evaluate_shebang(shebang: &str) -> String {
     match shebang {
-        _ if shebang.contains("bash") => "Bourne-Again shell",
-        _ if shebang.contains("tcsh") => "Tenex C shell",
-        _ if shebang.contains("csh") => "C shell",
-        _ if shebang.contains("yash") => "Yet-Another shell",
-        _ if shebang.contains("ash") => "Neil Brown's ash",
-        _ if shebang.contains("ksh") => "Korn shell",
-        _ if shebang.contains("zsh") => "Paul Falstad's zsh",
-        _ if shebang.contains("sh") => "POSIX shell",
-        _ if shebang.contains("python") => "Python",
+        _ if shebang.contains("bash") => ScriptType::Bash.as_str(),
+        _ if shebang.contains("tcsh") => ScriptType::Tcsh.as_str(),
+        _ if shebang.contains("csh") => ScriptType::Csh.as_str(),
+        _ if shebang.contains("yash") => ScriptType::Yash.as_str(),
+        _ if shebang.contains("ash") => ScriptType::Ash.as_str(),
+        _ if shebang.contains("ksh") => ScriptType::Ksh.as_str(),
+        _ if shebang.contains("zsh") => ScriptType::Zsh.as_str(),
+        _ if shebang.contains("sh") => ScriptType::Sh.as_str(),
+        _ if shebang.contains("python") => ScriptType::Python.as_str(),
         _ => shebang,
     }
     .to_owned()
@@ -54,18 +83,19 @@ fn evaluate_shebang(shebang: &str) -> String {
 #[cfg(test)]
 pub mod tests {
     use super::evaluate_shebang;
+    use crate::inside_file::ScriptType;
 
     #[test]
     fn test_evaluate_shebang() {
-        assert_eq!(evaluate_shebang("bash"), "Bourne-Again shell");
-        assert_eq!(evaluate_shebang("tcsh"), "Tenex C shell");
-        assert_eq!(evaluate_shebang("csh"), "C shell");
-        assert_eq!(evaluate_shebang("yash"), "Yet-Another shell");
-        assert_eq!(evaluate_shebang("ash"), "Neil Brown's ash");
-        assert_eq!(evaluate_shebang("ksh"), "Korn shell");
-        assert_eq!(evaluate_shebang("zsh"), "Paul Falstad's zsh");
-        assert_eq!(evaluate_shebang("sh"), "POSIX shell");
-        assert_eq!(evaluate_shebang("python"), "Python");
+        assert_eq!(evaluate_shebang("/lorem/bash"), ScriptType::Bash.as_str());
+        assert_eq!(evaluate_shebang("tcsh -ipsum"), ScriptType::Tcsh.as_str());
+        assert_eq!(evaluate_shebang("export l=s csh"), ScriptType::Csh.as_str());
+        assert_eq!(evaluate_shebang("amet/yash"), ScriptType::Yash.as_str());
+        assert_eq!(evaluate_shebang("ash --consec"), ScriptType::Ash.as_str());
+        assert_eq!(evaluate_shebang("adip; ksh"), ScriptType::Ksh.as_str());
+        assert_eq!(evaluate_shebang("elit/zsh"), ScriptType::Zsh.as_str());
+        assert_eq!(evaluate_shebang("sed && sh"), ScriptType::Sh.as_str());
+        assert_eq!(evaluate_shebang("do python c"), ScriptType::Python.as_str());
     }
 
     #[test]
